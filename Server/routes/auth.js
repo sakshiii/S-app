f7be7c72b90dc4981be0617678d2404614c24c5d
sakshiii <sakshikumsr53@gmail.com>
2020-11-04 12:03:14 +0530

    working

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express')
 //from express we'll use the router
 const router = express.Router()
- 
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
+
 router.get('/',(req,res)=>{
     res.send('Hiiiii Boooo')
 })
@@ -10,6 +12,26 @@ router.post('/signup',(req,res)=>{
      if(!email || !password || !name)
        return res.status(422).json({error:"Enter all the fields specified"})
 
-        res.json({message:"successfully posted"})
+        //res.json({message:"successfully posted"})
+        User.findOne({ email :  email })
+        .then((savedUser)=>{ 
+          if(savedUser)
+            return res.status(422).json({error:"User already exists with that email"})
+            const user = new User({
+              email,
+              password,
+              name
+            })
+
+
+        //else 
+        user.save()
+        .then (user=>{
+          res.json({message:"saved successfully"})
+        })
+        .catch(err=> {console.log(err)})            
+        })
+
+     .catch(err=>{ console.log(err)})   
 })
 module.exports = router
